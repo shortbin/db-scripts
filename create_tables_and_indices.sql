dcea931a278e31e636c9a93d10d8a51dc6efadb2
m4tu4g <71326926+m4tu4g@users.noreply.github.com>
2024-09-28 23:45:20 +0530

    fix: update schema of `users`, `urls`

--- create_tables_and_indices.sql
+++ create_tables_and_indices.sql
@@ -1,16 +1,16 @@
 -- Table `users`
 CREATE TABLE IF NOT EXISTS users
 (
-    id              UUID PRIMARY KEY,
-    email           VARCHAR(25) UNIQUE,
-    oauth_provider  VARCHAR(25),
-    oauth_uid       VARCHAR(50),
+    id              SERIAL PRIMARY KEY,
+    email           VARCHAR(30) UNIQUE NOT NULL,
+    -- oauth_provider  VARCHAR(25),
+    -- oauth_uid       VARCHAR(50),
     hashed_password TEXT NOT NULL,
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Indices for `users` table (not creating as UNIQUE idx is automatically created)
--- CREATE INDEX idx_users_email ON users USING HASH (email);
+-- CREATE INDEX idx_users_email_key ON users USING HASH (email);
 
 
 ------------------------------------------------------------
@@ -21,10 +21,11 @@ CREATE TABLE IF NOT EXISTS urls
 (
     short_id   VARCHAR(7) PRIMARY KEY,
     long_url   VARCHAR   NOT NULL,
-    user_id    UUID,
+    user_id    INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    expires_at TIMESTAMP NOT NULL
+    expires_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- Indices for `urls` table
-CREATE INDEX idx_urls_user_id ON urls USING HASH (user_id);
+CREATE INDEX idx_urls_userid_key ON urls (user_id); --USING HASH (user_id);
